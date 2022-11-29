c32ccaf72cb509cebee85bee1fb7d8ed6261e161
EC2 Default User <ec2-user@ip-172-31-40-251.ap-northeast-1.compute.internal>
2022-11-29 13:55:47 +0000

    test tcyuu

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
+devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   resources :books
   resources :users
 
   #resources :post_images, only: [:new, :index, :show]
 
-  get "/homes/about" => "homes#about", as: "about"
+  get "/home/about" => "homes#about", as: "about"
+
 end
