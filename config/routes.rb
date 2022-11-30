92b01712a56fd5a5caeff3ced435c36672919c90
EC2 Default User <ec2-user@ip-172-31-40-251.ap-northeast-1.compute.internal>
2022-11-30 00:28:56 +0000

    erroer kaisyo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-devise_scope :user do
+  devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
 end
   resources :books
